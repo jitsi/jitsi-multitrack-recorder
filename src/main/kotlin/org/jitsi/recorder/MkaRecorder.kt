569e3c9e01ce3f537f4d4dc06fce7a0c989fedcd
Boris Grozev <boris@jitsi.org>
2024-10-10 17:02:38 -0500

    Add some basic tests.

--- src/main/kotlin/org/jitsi/recorder/MkaRecorder.kt
+++ src/main/kotlin/org/jitsi/recorder/MkaRecorder.kt
@@ -65,59 +65,5 @@ class MkaRecorder(directory: File) {
     fun close() {
         writer.close()
         ioDW.close()
-        // testDocTraversal()
-        // destination.delete()
     }
-
-//    fun testDocTraversal() {
-//        val ioDS = FileDataSource(destination.path)
-//        val reader = EBMLReader(ioDS)
-//        var level0 = reader.readNextElement()
-//        while (level0 != null) {
-//            traverseElement(level0, ioDS, reader, 0)
-//            level0.skipData(ioDS)
-//            level0 = reader.readNextElement()
-//        }
-//    }
-//
-//    var x = 0
-//    private fun traverseElement(levelN: Element?, ioDS: DataSource, reader: EBMLReader, level: Int) {
-//        if (levelN == null) {
-//            return
-//        }
-//
-//        logger.info("Found element: ${".".repeat(level*2)} ${levelN.elementType.name}")
-//        if (levelN.elementType.name == "TimecodeScale") {
-//            levelN.readData(ioDS)
-//            println("oops: "+ levelN.data.get3Bytes())
-//            return
-//        }
-//        if (levelN.elementType.name == "Timecode") {
-//            if (x == 0) { x++} else {
-//                levelN.readData(ioDS)
-//                if (levelN.data.capacity() == 1)
-//                    println("oops: " + levelN.data.get().toInt())
-//                else if (levelN.data.capacity() == 2)
-//                    println("oops: " + levelN.data.getShort().toInt())
-//                else if (levelN.data.capacity() == 3)
-//                    println("oops: " + levelN.data.get3Bytes().toInt())
-//                else if (levelN.data.capacity() == 4)
-//                    println("oops: " + levelN.data.getInt())
-//                return
-//            }
-//        }
-//
-//        val elemLevel = levelN.elementType.level
-//        if (elemLevel != -1) {
-//            check(level.toLong() == elemLevel.toLong())
-//        }
-//        if (levelN is MasterElement) {
-//            var levelNPlusOne = levelN.readNextChild(reader)
-//            while (levelNPlusOne != null) {
-//                traverseElement(levelNPlusOne, ioDS, reader, level + 1)
-//                levelNPlusOne.skipData(ioDS)
-//                levelNPlusOne = levelN.readNextChild(reader)
-//            }
-//        }
-//    }
 }
--- src/test/kotlin/org/jitsi/recorder/MkaRecorderTest.kt
+++ src/test/kotlin/org/jitsi/recorder/MkaRecorderTest.kt
@@ -20,6 +20,12 @@ package org.jitsi.recorder
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.kotest.assertions.fail
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import org.ebml.EBMLReader
+import org.ebml.Element
+import org.ebml.MasterElement
+import org.ebml.io.DataSource
+import org.ebml.io.FileDataSource
 import org.jitsi.mediajson.Event
 import org.jitsi.mediajson.MediaEvent
 import org.jitsi.mediajson.StartEvent
@@ -33,7 +39,8 @@ class MkaRecorderTest : ShouldSpec() {
     val logger = createLogger()
 
     init {
-        val sample = "/sample.json"
+        val debug = true
+        val sample = "/opus-sample.json"
         val input = javaClass.getResource(sample)?.readText()?.lines()?.dropLast(1) ?: fail("Can not read $sample")
         val objectMapper = jacksonObjectMapper()
         val inputJson: List<Event> = input.map { objectMapper.readValue(it, Event::class.java) }
@@ -41,19 +48,75 @@ class MkaRecorderTest : ShouldSpec() {
 
         context("Recording") {
             val directory = Files.createTempDirectory("MkaRecorderTest").toFile()
-            logger.info("Recording to $directory")
+            val mkaFile = "$directory/recording.mka"
             val recorder = MkaRecorder(directory)
 
             inputJson.forEach {
                 if (it is StartEvent) {
-                    logger.info("Start new track: $it")
+                    logger.info("Starting new track: $it")
                     recorder.startTrack(it.start.tag)
                 } else if (it is MediaEvent) {
                     recorder.addFrame(it.media.tag, it.media.timestamp, Base64.decode(it.media.payload))
                 }
             }
-
             recorder.close()
+            logger.info("Recording completed.")
+
+            logger.info("Total EBML elements: ${traverseMka(mkaFile) { _ -> true } }")
+
+            // Expect as many Cluster elements as packets in the sample.
+            traverseMka(mkaFile) { element -> element.elementType.name == "Cluster" } shouldBe
+                    inputJson.count { it is MediaEvent }
+
+            if (debug) {
+                traverseMka(mkaFile) { element, level ->
+                    logger.info("${"  ".repeat(level)}${element.elementType.name}")
+                    true
+                }
+            }
         }
+    }
+
+    private fun traverseMka(path: String, match: (Element) -> Boolean) =
+        traverseMka(path) { element, _ -> match(element) }
+
+    private fun traverseMka(path: String, match: (Element, Int) -> Boolean): Int {
+        val ioDS = FileDataSource(path)
+        val reader = EBMLReader(ioDS)
+        var level0 = reader.readNextElement()
+        var count = 0
+        while (level0 != null) {
+            if (match(level0, 0)) count++
+            count += traverseElement(level0, ioDS, reader, 0, match)
+            level0.skipData(ioDS)
+            level0 = reader.readNextElement()
+        }
+        return count
+    }
+
+    private fun traverseElement(
+        element: Element,
+        ioDS: DataSource,
+        reader: EBMLReader,
+        level: Int,
+        match: (Element, Int) -> Boolean = { _, _ -> true }
+    ): Int {
+        var count = 0
+        if (match(element, level)) count++
+
+        val elemLevel = element.elementType.level
+        if (elemLevel != -1) {
+            check(level.toLong() == elemLevel.toLong())
+        }
+        if (element is MasterElement) {
+            var levelNPlusOne = element.readNextChild(reader)
+            while (levelNPlusOne != null) {
+                count += traverseElement(levelNPlusOne, ioDS, reader, level + 1, match)
+                levelNPlusOne.skipData(ioDS)
+                levelNPlusOne = element.readNextChild(reader)
+            }
+        }
+
+        return count
     }
 }
