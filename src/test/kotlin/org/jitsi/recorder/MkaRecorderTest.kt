efe63602b594263e71a701a761e450aed5a366a6
Boris Grozev <boris@jitsi.org>
2024-10-10 12:25:42 -0500

    Add license and headers.

--- src/test/kotlin/org/jitsi/recorder/MkaRecorderTest.kt
+++ src/test/kotlin/org/jitsi/recorder/MkaRecorderTest.kt
@@ -1,3 +1,20 @@
+/*
+ * Jitsi Multi Track Recorder
+ *
+ * Copyright @ 2024-Present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jitsi.recorder
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -11,7 +28,7 @@ import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
 
 @OptIn(ExperimentalEncodingApi::class)
-class RecorderTest : ShouldSpec(){
+class MkaRecorderTest : ShouldSpec(){
     init {
         val sample = "/opus-sample3.json"
         val input = javaClass.getResource(sample)?.readText()?.lines()?.dropLast(1) ?: fail("Can not read $sample")
